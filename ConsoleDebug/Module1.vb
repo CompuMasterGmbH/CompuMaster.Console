1e641f9e03f575b2b7c64e1a2d677a18f8d940e4
Jochen Wezel <jwezel@compumaster.de>
2017-10-11 14:27:16 +0200

    added sample code and screenshots of sample output

--- ConsoleDebug/Module1.vb
+++ ConsoleDebug/Module1.vb
@@ -1,7 +1,22 @@
 ﻿Module Module1
 
     Sub Main()
+        'Create some wonderful output to the console
+        CompuMaster.Console.Write("Hello ")
+        CompuMaster.Console.WriteLine("World!")
 
+        CompuMaster.Console.ForegroundColor = System.ConsoleColor.White
+        CompuMaster.Console.BackgroundColor = System.ConsoleColor.Blue
+        CompuMaster.Console.WriteLine("This is white text on blue background.")
+        CompuMaster.Console.WriteLine("This is another yellow text on gray background.", System.ConsoleColor.Yellow, System.ConsoleColor.DarkGray)
+        CompuMaster.Console.ResetColor()
+
+        CompuMaster.Console.Warn("WARNING: ")
+        CompuMaster.Console.OkayLine("This nice sample has completed!")
+
+        'Write log to disk
+        CompuMaster.Console.SavePlainTextLog("sample-log.txt")
+        CompuMaster.Console.SaveHtmlLog("sample-log.html")
     End Sub
 
 End Module
