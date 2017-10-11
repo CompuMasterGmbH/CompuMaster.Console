e08b4cca53bcc05686f710d61380ba2ccf6586d4
Jochen Wezel <jwezel@compumaster.de>
2017-10-11 14:38:00 +0200

    increased verison no.

--- Console/AssemblyInfo.vb
+++ Console/AssemblyInfo.vb
@@ -6,11 +6,11 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("Adds additional logging capabilities to the System.Console")>
 <Assembly: AssemblyCompany("CompuMaster GmbH")>
 <Assembly: AssemblyProduct("CompuMaster.Console")>
-<Assembly: AssemblyCopyright("2017 CompuMaster GmbH")>
+<Assembly: AssemblyCopyright("2016-2017 CompuMaster GmbH")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 <Assembly: Guid("4abc0102-aa9c-4f3b-89ec-7734a79f667c")>
 
-<Assembly: AssemblyVersion("2017.10.11.100")>
-<Assembly: AssemblyInformationalVersion("2017.10.11.100-beta")>
+<Assembly: AssemblyVersion("2017.10.11.101")>
+'<Assembly: AssemblyInformationalVersion("2017.10.11.100-beta")>
