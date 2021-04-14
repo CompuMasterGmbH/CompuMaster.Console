8d2d62e64853811f072505d6c35741c7a2783dd3
Thomas Schädler - CompuMaster GmbH <schaedler@lan.compumaster.de>
2021-04-14 15:48:03 +0200

    reaname folder from "CompuMaster.Console" to "Console"
    -change path to CompuMaster.Console.vbproj-file
    -change project reference

---
+++ Console/Console.ControlCKeyPressedException.vb
@@ -0,0 +1,55 @@
+﻿Option Explicit On
+Option Strict On
+
+Namespace CompuMaster
+
+    Partial Public Class Console
+
+        ''' <summary>
+        ''' An exception that is fired when the user has pressed the key combination Control-C
+        ''' </summary>
+        Public Class ControlCKeyPressedException
+            Inherits Exception
+
+            Friend Sub New()
+                MyBase.New()
+            End Sub
+
+            ''' <summary>
+            ''' Internal workaround to force stack trace to show up with origin line numbers after re-throwing the origin exception
+            ''' </summary>
+            ''' <param name="innerException"></param>
+            Friend Sub New(innerException As ControlCKeyPressedException)
+                MyBase.New("", innerException)
+            End Sub
+
+            Public Overrides ReadOnly Property StackTrace As String
+                Get
+                    If Me.InnerException IsNot Nothing AndAlso GetType(ControlCKeyPressedException).IsInstanceOfType(Me.InnerException) Then
+                        Return Me.InnerException.StackTrace
+                    Else
+                        Return MyBase.StackTrace
+                    End If
+                End Get
+            End Property
+            Public Overrides ReadOnly Property Message As String
+                Get
+                    If Me.InnerException IsNot Nothing AndAlso GetType(ControlCKeyPressedException).IsInstanceOfType(Me.InnerException) Then
+                        Return Me.InnerException.Message
+                    Else
+                        Return MyBase.Message
+                    End If
+                End Get
+            End Property
+            Public Overrides Function ToString() As String
+                If Me.InnerException IsNot Nothing AndAlso GetType(ControlCKeyPressedException).IsInstanceOfType(Me.InnerException) Then
+                    Return Me.InnerException.ToString
+                Else
+                    Return MyBase.ToString
+                End If
+            End Function
+        End Class
+
+    End Class
+
+End Namespace
