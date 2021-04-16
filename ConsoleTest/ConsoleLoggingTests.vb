bbc793b08c38a9f0968e928af9157634dbc5c0ee
Jochen Wezel <jwezel@compumaster.de>
2021-04-16 18:08:55 +0200

    added some real tests and a test failure for checking test results output in github workflows

---
+++ ConsoleTest/ConsoleLoggingTests.vb
@@ -0,0 +1,107 @@
+Imports NUnit.Framework
+
+Namespace ConsoleTest
+
+    Public Class ConsoleLoggingTests
+
+        <SetUp>
+        Public Sub Setup()
+            CompuMaster.Console.Clear(True, True, True, True)
+        End Sub
+
+        <Test> Public Sub TestMustFailForTestingGitHubWorkflowsTestDisplay()
+            Assert.Fail("This is a demo test failure")
+        End Sub
+
+        <Test>
+        Public Sub LogAndWarningsOutput()
+            'Create some wonderful output to the console
+            CompuMaster.Console.Write("Hello ")
+            CompuMaster.Console.WriteLine("World!")
+
+            CompuMaster.Console.ForegroundColor = System.ConsoleColor.White
+            CompuMaster.Console.BackgroundColor = System.ConsoleColor.Blue
+            CompuMaster.Console.WriteLine("This is white text on blue background.")
+            CompuMaster.Console.WriteLine("This is another yellow text on gray background.", System.ConsoleColor.Yellow, System.ConsoleColor.DarkGray)
+            CompuMaster.Console.WriteLine()
+            CompuMaster.Console.ResetColor()
+
+            CompuMaster.Console.WarnLine("WARNING: Now, let's see the indentation feature")
+            CompuMaster.Console.CurrentIndentationLevel += 1
+            CompuMaster.Console.Write("Okay, let's start indentation: ")
+            CompuMaster.Console.WriteLine("This is a text with " & CompuMaster.Console.CurrentIndentationLevel & " levels of indentation")
+            CompuMaster.Console.CurrentIndentationLevel += 1
+            CompuMaster.Console.Write("It's time to continue: ")
+            CompuMaster.Console.WriteLine("This is a text with " & CompuMaster.Console.CurrentIndentationLevel & " levels of indentation")
+            CompuMaster.Console.Log("Please note: ")
+            CompuMaster.Console.LogLine("This line will only appear in log, but with " & CompuMaster.Console.CurrentIndentationLevel & " levels of indentation")
+            CompuMaster.Console.CurrentIndentationLevel += 1
+            CompuMaster.Console.Write("Yeah, get some more indentation: ")
+            CompuMaster.Console.WriteLine("This is a text with " & CompuMaster.Console.CurrentIndentationLevel & " levels of indentation")
+            CompuMaster.Console.CurrentIndentationLevel = 1
+            CompuMaster.Console.WriteLine("This is a multiline text, line 1" & ControlChars.CrLf & "This is a multiline text, line 2" & ControlChars.Cr & "This is a multiline text, line 3" & ControlChars.Lf & ControlChars.CrLf & "This is a multiline text, line 1 of paragraph 2" & ControlChars.CrLf & "This is a multiline text, line 2 of paragraph 2")
+            CompuMaster.Console.WriteLine()
+            CompuMaster.Console.CurrentIndentationLevel = 0
+
+            CompuMaster.Console.OkayLine("OKAY: This nice sample has completed!")
+
+            'Write log to disk
+            CompuMaster.Console.SavePlainTextLog("sample-log.txt")
+            CompuMaster.Console.SaveHtmlLog("sample-log.html")
+            CompuMaster.Console.SaveWarningsPlainTextLog("sample-warnings.txt")
+            CompuMaster.Console.SaveWarningsHtmlLog("sample-warnings.html")
+
+            'Compare results
+            Dim Expected As String
+            Dim Result As String
+
+            Result = System.IO.File.ReadAllText("sample-warnings.txt")
+            Expected =
+                "WARNING: Now, let's see the indentation feature" & System.Environment.NewLine
+            Assert.AreEqual(Expected, Result)
+
+            Result = System.IO.File.ReadAllText("sample-log.txt")
+            Expected =
+                "Hello World!" & System.Environment.NewLine &
+                "This is white text on blue background." & System.Environment.NewLine &
+                "This is another yellow text on gray background." & System.Environment.NewLine &
+                "" & System.Environment.NewLine &
+                "WARNING: Now, let's see the indentation feature" & System.Environment.NewLine &
+                "Okay, let's start indentation: This is a text with 1 levels of indentation" & System.Environment.NewLine &
+                "It's time to continue: This is a text with 2 levels of indentation" & System.Environment.NewLine &
+                "Please note: This line will only appear in log, but with 2 levels of indentation" & System.Environment.NewLine &
+                "Yeah, get some more indentation: This is a text with 3 levels of indentation" & System.Environment.NewLine &
+                "This is a multiline text, line 1" & System.Environment.NewLine &
+                "    This is a multiline text, line 2" & System.Environment.NewLine &
+                "    This is a multiline text, line 3" & System.Environment.NewLine &
+                "    " & System.Environment.NewLine &
+                "    This is a multiline text, line 1 of paragraph 2" & System.Environment.NewLine &
+                "    This is a multiline text, line 2 of paragraph 2" & System.Environment.NewLine &
+                "" & System.Environment.NewLine &
+                "OKAY: This nice sample has completed!" & System.Environment.NewLine
+            Assert.AreEqual(Expected, Result)
+
+            Result = System.IO.File.ReadAllText("sample-warnings.html")
+            Console.WriteLine(Result)
+            Assert.IsTrue(Result.StartsWith("<html>" & System.Environment.NewLine & "<head><title>"))
+            Result = Result.Substring(Result.IndexOf("</head>") + "</head>".Length)
+            Expected = System.Environment.NewLine &
+                "<body style=""background-color: Black;"">" & System.Environment.NewLine &
+                "<span style=""color: LightGray;"">WARNING: Now, let&#39;s see the indentation feature<br /></span>" & System.Environment.NewLine &
+                "</body></html>"
+            Assert.AreEqual(Expected, Result)
+
+            Result = System.IO.File.ReadAllText("sample-log.html")
+            Console.WriteLine(Result)
+            Assert.IsTrue(Result.StartsWith("<html>" & System.Environment.NewLine & "<head><title>"))
+            Result = Result.Substring(Result.IndexOf("</head>") + "</head>".Length)
+            Expected = System.Environment.NewLine &
+                "<body style=""background-color: Black;"">" & System.Environment.NewLine &
+                "<span style=""color: LightGray;""><span style=""color: White;"">Hello&nbsp;</span><span style=""color: White;"">World!<br /></span><span style=""background-color: Blue;""><span style=""color: White;"">This&nbsp;is&nbsp;white&nbsp;text&nbsp;on&nbsp;blue&nbsp;background.<br /></span></span><span style=""background-color: DarkGray;""><span style=""color: Yellow;"">This&nbsp;is&nbsp;another&nbsp;yellow&nbsp;text&nbsp;on&nbsp;gray&nbsp;background.<br /></span></span><br /><span style=""color: Red;"">WARNING:&nbsp;Now,&nbsp;let&#39;s&nbsp;see&nbsp;the&nbsp;indentation&nbsp;feature<br /></span>Okay,&nbsp;let&#39;s&nbsp;start&nbsp;indentation:&nbsp;This&nbsp;is&nbsp;a&nbsp;text&nbsp;with&nbsp;1&nbsp;levels&nbsp;of&nbsp;indentation<br />It&#39;s&nbsp;time&nbsp;to&nbsp;continue:&nbsp;This&nbsp;is&nbsp;a&nbsp;text&nbsp;with&nbsp;2&nbsp;levels&nbsp;of&nbsp;indentation<br />Please&nbsp;note:&nbsp;This&nbsp;line&nbsp;will&nbsp;only&nbsp;appear&nbsp;in&nbsp;log,&nbsp;but&nbsp;with&nbsp;2&nbsp;levels&nbsp;of&nbsp;indentation<br />Yeah,&nbsp;get&nbsp;some&nbsp;more&nbsp;indentation:&nbsp;This&nbsp;is&nbsp;a&nbsp;text&nbsp;with&nbsp;3&nbsp;levels&nbsp;of&nbsp;indentation<br />This&nbsp;is&nbsp;a&nbsp;multiline&nbsp;text,&nbsp;line&nbsp;1<br />&nbsp;&nbsp;&nbsp;&nbsp;This&nbsp;is&nbsp;a&nbsp;multiline&nbsp;text,&nbsp;line&nbsp;2<br />&nbsp;&nbsp;&nbsp;&nbsp;This&nbsp;is&nbsp;a&nbsp;multiline&nbsp;text,&nbsp;line&nbsp;3<br />&nbsp;&nbsp;&nbsp;&nbsp;<br />&nbsp;&nbsp;&nbsp;&nbsp;This&nbsp;is&nbsp;a&nbsp;multiline&nbsp;text,&nbsp;line&nbsp;1&nbsp;of&nbsp;paragraph&nbsp;2<br />&nbsp;&nbsp;&nbsp;&nbsp;This&nbsp;is&nbsp;a&nbsp;multiline&nbsp;text,&nbsp;line&nbsp;2&nbsp;of&nbsp;paragraph&nbsp;2<br /><br /><span style=""color: Green;"">OKAY:&nbsp;This&nbsp;nice&nbsp;sample&nbsp;has&nbsp;completed!<br /></span></span>" & System.Environment.NewLine &
+                "</body></html>"
+            Assert.AreEqual(Expected, Result)
+        End Sub
+
+    End Class
+
+End Namespace
